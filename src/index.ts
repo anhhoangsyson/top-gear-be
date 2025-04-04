0042d3801a8c549face0f2cd97ad7b4116071612
anhhoangsyson <anhhoangsyson@gmail.com>
2025-04-04 23:00:21 +0700

    change core url for localhost for dev

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: ['http://localhost:3001'],
+    origin: ['http://localhost:3000'],
     allowedHeaders: 'Content-Type,Authorization',
   }),
 );
